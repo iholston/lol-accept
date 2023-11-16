57f7779e66393a82af5f422c3e1a17379e5f4dc0
Isaac Holston <isaac.holston@outlook.com>
2023-11-15 19:20:22 -0700

    fix: update executable icon path

--- tray.py
+++ tray.py
@@ -1,4 +1,6 @@
 import pystray
+import os
+import sys
 from PIL import Image
 
 
@@ -7,13 +9,19 @@ class TrayIcon:
     def __init__(self, acceptor):
         self.name = "Heimerdinger"
         self.display_name = "Heimerdinger"
-        self.image = Image.open("icon.png")
+        self.icon = "icon.png"
+        self.image = Image.open(self.resource_path())
         self.start_pause = "Pause"
         self.acceptor = acceptor
         self.start_pause_item = pystray.MenuItem(lambda text: self.start_pause, self.pause)
         self.exit_item = pystray.MenuItem("Exit", self.exit)
         self.menu = pystray.Menu(self.start_pause_item, self.exit_item)
         self.icon = pystray.Icon(self.name, self.image, self.display_name, self.menu)
+
+    def resource_path(self):
+        if hasattr(sys, '_MEIPASS'):
+            return os.path.join(sys._MEIPASS, self.icon)
+        return os.path.join(os.path.abspath("."), self.icon)
 
     def run(self):
         self.acceptor.run()
